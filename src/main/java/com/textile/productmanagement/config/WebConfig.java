e2ef77970b2a1c3f68982e6221f9d518eff32a64
suresh <ccsuresh120@gmail.com>
2026-03-23 16:41:26 +0530

    fix cors

--- src/main/java/com/textile/productmanagement/config/WebConfig.java
+++ src/main/java/com/textile/productmanagement/config/WebConfig.java
@@ -5,15 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://mango-glacier-0f925fc00.2.azurestaticapps.net"
-                )
+                .allowedOrigins("https://mango-glacier-0f925fc00.2.azurestaticapps.net")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
